c99537bb8aef2492c1b555d9fe53c6265be10978
Akhlak Hossain Jim <73884856+Akhlak-Hossain-Jim@users.noreply.github.com>
2021-04-21 15:37:35 +0600

    Update script.js

--- script.js
+++ script.js
@@ -2,15 +2,28 @@ function compute() {
   var principal = document.getElementById("principal").value;
   var rate = document.getElementById("rate").value;
   var years = document.getElementById("years").value;
-  console.log(years);
-  var interest = (principal * years * rate) / 100;
-  var year = new Date().getFullYear() + parseInt(years);
-  var result = document.getElementById("result");
-  result.innerHTML = `  <br />
-                        <br />If you deposit <mark>${principal}</mark>,<br />
-                        at an interest rate of <mark>${rate}</mark>,<br />
-                        You will receive an amount of <mark>${interest}</mark>,<br />
-                        in the year <mark>${year}</mark>`;
+
+  if (principal == "") {
+    alert("Please enter the name");
+    username.focus();
+    return false;
+  }
+
+  if (rate == 0) {
+    alert("Please enter the name");
+    username.focus();
+    return false;
+  } else {
+    var interest = (principal * years * rate) / 100;
+    var year = new Date().getFullYear() + parseInt(years);
+    var result = document.getElementById("result");
+
+    result.innerHTML = `  <br />
+                          <br />If you deposit <mark>${principal}</mark>,<br />
+                          at an interest rate of <mark>${rate}</mark>,<br />
+                          You will receive an amount of <mark>${interest}</mark>,<br />
+                          in the year <mark>${year}</mark>`;
+  }
 }
 function updateRate() {
   var rateval = document.getElementById("rate").value;
